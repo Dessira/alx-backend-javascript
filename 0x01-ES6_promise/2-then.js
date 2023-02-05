c1296de24fb0d76aab39664c3fdb38683bc29bd3
Desire Barine <desirebarine16@gmail.com>
2023-02-05 21:09:40 +0100

    linter and error handling fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,10 @@
-export default function handleResponseFromAPI(promise){
-	promise.then( data => {
-		console.log("Got a response from the API");
-		return {
-			status: 200,
-			body: "Success"
-		}
-	})
-		.catch(err =>{
-			return {}
-		});
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'Success',
+    };
+  })
+    .catch(() => new Error({}));
 }
